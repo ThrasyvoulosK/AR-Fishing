08c2789715b7a25811028e30acc7c57223a71540
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-06-09 00:07:50 +0300

    fixes sphere position

--- Assets/SphereScript.cs
+++ Assets/SphereScript.cs
@@ -6,21 +6,36 @@ public class SphereScript : MonoBehaviour
 {
     //public float waterHeight = 2.39f;
     public float waterHeight = 2f;
+    public float heightFactor=0.1f;
     public bool isCastedCorrectly=false;
     // Start is called before the first frame update
     void Start()
     {
          waterHeight = 2f;
-     isCastedCorrectly = false;
-}
+        heightFactor = 0.1f;
+        isCastedCorrectly = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (isCastedCorrectly && gameObject.transform.position.y > waterHeight)
-            gameObject.transform.Translate(Vector3.down * Time.deltaTime);
-        else if (isCastedCorrectly && gameObject.transform.position.y < waterHeight)
-            gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+        Debug.Log("tp" + gameObject.transform.position.y);
+        if (isCastedCorrectly && (gameObject.transform.position.y > (waterHeight+heightFactor)))
+        {
+            Debug.Log("y>water"+ (gameObject.transform.position.y > waterHeight));
+            //gameObject.transform.Translate(Vector3.down * Time.deltaTime);
+            gameObject.transform.Translate(transform.up * Time.deltaTime);
+        }
+        else if (isCastedCorrectly && (gameObject.transform.position.y < (waterHeight-heightFactor)))
+        {
+            Debug.Log("y<water" + (gameObject.transform.position.y < waterHeight));
+            //gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+            gameObject.transform.Translate(-transform.up * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Stable Sphere");
+        }
         
         //Debug.Log("tp"+transform.position);
         //Debug.Log("tpl"+transform.localPosition);
