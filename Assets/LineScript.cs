5c37377d97b5cfe75e095a75a27533f699b4f671
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-06-30 23:33:55 +0300

    Adds Line Rendering

---
+++ Assets/LineScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    [SerializeField]
+    Transform startingPoint;
+    [SerializeField]
+    Transform endingPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, startingPoint.position);
+        lineRenderer.SetPosition(1, endingPoint.position);
+    }
+}
