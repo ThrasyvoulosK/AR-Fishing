b914ec0c5e503f9fbfdbc72b84c55ed854687cae
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-07-07 19:07:04 +0300

    Adds Main Menu

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //load the first available scene
+    public void PlayLevel()
+    {
+        //TEMP: Currently there's only one scene!
+        SceneManager.LoadScene(1);
+    }
+}
