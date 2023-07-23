9304a7d83007844c020faba0200b6d6ed3a2d305
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-07-24 00:33:07 +0300

    Changes level select screen
    to appear in full on 'portrait' play

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVJUNXVHYi8vLy8vQUFBQm1XbFFiczdTdzBUUGhZeGNUU1NzQ05ZOXBZc2YrWWVXWm0vOUI0d1lsRFBSZnZodnVLaStSMmVDM0FGNitEZENyZy93L28yWUdCZFNuYTAwRkg2WTF0d1pOTDRwU3ovc0hsVk9tNzdvYnRFS1RzTTk5dWF6OU5BWS8wSWFhcjBoeXdndmxnTlZBY3VBRVJCS1JRb3dSTE5GOWU2ZitjWEd4WVErTit0bldCSkFtbGN4QnYzc0RsQWhQNmt3ZnBOQVFpb0dLdTNpMjFwTkdUSGpabHRuNDFwVmlyOTRGc0w1UkVpWlFuSUh0UnB6eGlOVmt3Z1h0ZFpkaWRGNldhQ2tiY3grOUJUK3htV2kxd2JMRXhraS9mZFdxSVR6Nkk5UVZsWk56eG1uZU9HZG56c1MrbUVyZklVTE1aTk5Idm1iWTdyc2dUYXVDRWx0QndKYzhvM3NoM0toZWZzUXhmVUxNUDZXVTh2TFRNcFQ=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
