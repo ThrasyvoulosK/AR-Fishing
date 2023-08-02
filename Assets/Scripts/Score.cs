012cbcc8cf620e7affd84cc02599c0f16d6d5d28
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-08-03 01:42:34 +0300

    Makes Score visible in-game

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 //keeps a 'score' value, that is also saved in PlayerPrefs
@@ -7,11 +8,18 @@ public class Score : MonoBehaviour
 {
     public int score;
     public string scoreName = "score";
+    TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         score = PlayerPrefs.GetInt(scoreName, 0);
+        scoreText = GameObject.Find("Canvas").transform.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        scoreText.SetText("ScorePoints: \n" + score);
     }
 
     //add a number to the current value
